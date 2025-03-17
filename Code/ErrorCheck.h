501a167657217f30974733390352ecb99a87beb4
Gianni <107214928+Gio-Dra@users.noreply.github.com>
2025-03-17 12:14:22 -0700

    Update ErrorCheck.h
    Ooopsie didnt see the cpp file

--- Code/ErrorCheck.h
+++ Code/ErrorCheck.h
@@ -2,11 +2,4 @@
 #include <string>
 using namespace std;
 
-void errorCheckInt(int &x, int min, int max, string prompt) { // Daniels Code
-    while (cin.fail() || x < min || x > max) {               // G Code \|/
-        cin.clear();
-        cin.ignore(1000, '\n');
-        cout << "Invalid input. " << prompt;
-        cin >> x;
-    }
-}
+void errorCheckInt(int &x, int min, int max, string prompt) {}

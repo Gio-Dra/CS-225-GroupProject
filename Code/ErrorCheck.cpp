51523e66c8b92db632c02106e0dbeeef9bf31e7e
theCoderDude575 <200008181+theCoderDude575@users.noreply.github.com>
2025-03-27 14:50:46 -0700

    ErrorCheck files updated

--- Code/ErrorCheck.cpp
+++ Code/ErrorCheck.cpp
@@ -1,14 +1,30 @@
 #include <iostream>
-#include <string>
-#include <climits>
 #include "ErrorCheck.h"
+
 using namespace std;
 
-void errorCheckInt(int &x, int min, int max, string prompt) {
-    while ((!cin)||((x < min)||(x > max))) {
-        cin.clear();
-        cin.ignore(INT_MAX, '\n');
-        cout << "Error, try again." << endl << prompt;
-        cin >> x;
+// Validates input: must be a number and within [min, max] range
+void errorCheckInt(int &x, int min, int max, const string &prompt) {
+    bool valid = false;
+
+    while (!valid) {
+        try {
+            if (cin.fail()) {
+                cin.clear();
+                cin.ignore(1000, '\n');
+                throw "Input must be a number.";
+            }
+
+            if (x < min || x > max) {
+                throw "Input is out of range.";
+            }
+
+            valid = true; // If we pass both checks, input is valid
+
+        } catch (const char* msg) {
+            cout << "Error: " << msg << endl;
+            cout << prompt;
+            cin >> x; // Retry
+        }
     }
 }
--- Code/ErrorCheck.h
+++ Code/ErrorCheck.h
@@ -1,30 +1,4 @@
-#include <iostream>
 #include <string>
 
-using namespace std;
-
 // Validates input: must be a number and within [min, max] range
-void errorCheckInt(int &x, int min, int max, const string &prompt) {
-    bool valid = false;
-
-    while (!valid) {
-        try {
-            if (cin.fail()) {
-                cin.clear();
-                cin.ignore(1000, '\n');
-                throw "Input must be a number.";
-            }
-
-            if (x < min || x > max) {
-                throw "Input is out of range.";
-            }
-
-            valid = true; // If we pass both checks, input is valid
-
-        } catch (const char* msg) {
-            cout << "Error: " << msg << endl;
-            cout << prompt;
-            cin >> x; // Retry
-        }
-    }
-}
+void errorCheckInt(int &x, int min, int max, const string &prompt);
